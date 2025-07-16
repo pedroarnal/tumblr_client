635195ec65d5756f8fa4a741bf96613f5619784a
Pedro Arnal Puente <pedroarnal@agitdev.com>
2025-07-16 23:10:01 +0200

    Add frozen string literals to all library files.
    This optimization improves performance and memory usage by preventing
    string mutations and reducing allocations in Ruby 3.4.

--- lib/tumblr/client.rb
+++ lib/tumblr/client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'tumblr/blog'
 require 'tumblr/user'
 require 'tumblr/request'
--- lib/tumblr_client.rb
+++ lib/tumblr_client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'tumblr/client'
 require 'tumblr/config'
 
